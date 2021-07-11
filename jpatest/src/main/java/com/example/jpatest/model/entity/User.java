e0007caa0bceed8571393413c36b98563ffab01e
youngkyunIm <yee0230@naver.com>
2021-07-11 22:05:40 +0900

    ing

--- jpatest/src/main/java/com/example/jpatest/model/entity/User.java
+++ jpatest/src/main/java/com/example/jpatest/model/entity/User.java
@@ -1,7 +1,10 @@
 package com.example.jpatest.model.entity;
 
+import javax.persistence.Entity;
+
 import lombok.Data;
 
+@Entity
 @Data
 public class User {
     private Long id;
--- jpatest/src/main/java/com/example/jpatest/repository/UserRepositoty.java
+++ jpatest/src/main/java/com/example/jpatest/repository/UserRepositoty.java
@@ -1,8 +1,10 @@
 package com.example.jpatest.repository;
 
+import com.example.jpatest.model.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepositoty extends JpaRepository<User, Long>{
-   
 }
